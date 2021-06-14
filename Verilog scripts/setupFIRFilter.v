788eed075269aab252392f7b5d3f7affce06a73d
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-14 13:17:04 +0100

    Added the inputs/outputs and parameters of the new module.

--- Verilog scripts/setupFIRFilter.v
+++ Verilog scripts/setupFIRFilter.v
@@ -1,6 +1,13 @@
-module setupFIRFilter(
+module setupFIRFilter #(
+	parameter LENGTH = 10,
+	parameter DATA_WIDTH = 8
+)
 
-
+(
+	input clock,
+	input enable,
+	output reg filterSetFlag,
+	output reg signed [DATA_WIDTH - 1:0] coefficient_in;
 );
 
 
