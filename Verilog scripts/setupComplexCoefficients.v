93edbd0300473aa75c5471eaa2bdfb30c7340892
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-16 09:00:01 +0100

    Copied the logic of setupCoefficients to this script. This is practically a copy and paste with any coefficients variables being changed coefficientsRe and coefficientsIm.

--- Verilog scripts/setupComplexCoefficients.v
+++ Verilog scripts/setupComplexCoefficients.v
@@ -11,4 +11,59 @@ module setupComplexCoefficients#(
 
 
 
+// Coefficient counter. Filter will only for 1023 ((2^10)-1) taps.
+reg [9:0] coeffCounter;
+// Designing the correct length of the coefficient arrays based on parameters LENGTH and DATA_WIDTH.
+reg signed [DATA_WIDTH - 1:0] coefficientsRe [0:LENGTH - 1];
+reg signed [DATA_WIDTH - 1:0] coefficientsIm [0:LENGTH - 1];
+
+
+// Setting the coefficients. When setting the coefficients, make sure all values are covered.
+// This should include from 0 to LENGTH - 1.
+initial begin
+
+end
+
+
+// Set the initial outputs to 0.
+initial begin
+	coefficientOutRe <= {(DATA_WIDTH){1'd0}};
+	coefficientOutIm <= {(DATA_WIDTH){1'd0}};
+	filterSetFlag <= 1'd0;
+	coeffCounter <= 10'd0;
+end
+
+
+
+always @(posedge clock) begin
+
+	// If enable is set, set coefficientOut based on the coeffCounter and the array coefficients values.
+	// When all the coefficients were passed across, set the filterSetFlag high. If not enabled, set 
+	// filterSetFlag low and reset the coeffCounter.
+	if(enable) begin: setCoefficients
+
+		// Set coefficientOut to the corresponding coefficients array value.
+		coefficientOutRe <= coefficientsRe[coeffCounter];
+		coefficientOutIm <= coefficientsIm[coeffCounter];
+		
+		// Increment coeffCounter each loop.
+		coeffCounter <= coeffCounter + 10'd1;
+		
+		// Set flag high when coeffCounter is equal to the filter length - 1.
+		if(coeffCounter == LENGTH - 1) begin
+			filterSetFlag <= 1'd1;
+		end
+		
+	end
+	else begin
+		filterSetFlag <= 1'd0;
+		coeffCounter <= 10'd0;
+		coefficientOutRe <= {(DATA_WIDTH){1'd0}};
+		coefficientOutIm <= {(DATA_WIDTH){1'd0}};
+	end
+	
+end
+
+
 endmodule
+
