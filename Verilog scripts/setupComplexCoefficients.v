9b4b13573164d83ad582dbbd2a9af2d3f4444afb
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-16 08:55:03 +0100

    Added the input and output ports for the module.

--- Verilog scripts/setupComplexCoefficients.v
+++ Verilog scripts/setupComplexCoefficients.v
@@ -1,6 +1,12 @@
-module setupComplexCoefficients)(
-
-
+module setupComplexCoefficients#(
+	parameter LENGTH = 20,
+	parameter DATA_WIDTH = 8
+)(
+	input clock,
+	input enable,
+	output reg filterSetFlag,
+	output reg signed [DATA_WIDTH - 1:0] coefficientOutRe,
+	output reg signed [DATA_WIDTH - 1:0] coefficientOutIm
 );
 
 
