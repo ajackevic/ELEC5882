cdcb584d5132b7c47ca183205fcde064c37936d0
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-16 10:20:10 +0100

    Copied the logic of n-tap_fir_tb to this script. Its near identical. Also created a backup of this file.

--- Verilog scripts/complex_n_tap_fir/n_tap_complex_fir_tb.v
+++ Verilog scripts/complex_n_tap_fir/n_tap_complex_fir_tb.v
@@ -8,9 +8,9 @@
  Module Description:
  -------------------
  This module is a test bench for the module n_tap_complex_fir.v. The script
- sends the coefficients (coefficient_in_I and coefficient_in_Q) and the
- input data (sr_data_in_I and sr_data_in_Q) to the test script, the output
- data (data_out_I and data_out_Q) is then observed in ModelSim. The results
+ sends the coefficients (coefficientInRe and coefficientInIm) and the
+ input data (srDataInRe and srDataInIm) to the test script, the output
+ data (dataOutRe and dataOutIm) is then observed in ModelSim. The results
  are then confirmed through the convolution operation in MATLAB, with the same
  inputs.
 
@@ -24,34 +24,34 @@ localparam CLOCK_FREQ = 50000000;
 localparam RST_CYCLES = 10;
 
 reg clock;
-reg load_coefficients_flag;
-reg load_data_flag;
-reg stop_data_load_flag;
+reg loadCoefficientsFlag;
+reg loadDataFlag;
+reg stopDataLoadFlag;
 
-reg signed [7:0] coefficient_in_I;
-reg signed [7:0] coefficient_in_Q;
-reg signed [7:0] sr_data_in_I;
-reg signed [7:0] sr_data_in_Q;
+reg signed [7:0] coefficientInRe;
+reg signed [7:0] coefficientInIm;
+reg signed [7:0] srDataInRe;
+reg signed [7:0] srDataInIm;
 
 // Note the range of reg signed [7:0] is [-128 to 127].
 
-wire [20:0] data_out_I;
-wire [20:0] data_out_Q;
+wire [20:0] dataOutRe;
+wire [20:0] dataOutIm;
 
 // Connect the device under test
 n_tap_complex_fir #(
-	.length			(4)
+	.length					(12),
+	.DATA_WIDTH				(8)
 	) dut(
 	.clock					(clock),
-	.load_coefficients_flag (load_coefficients_flag),
-	.load_data_flag			(load_data_flag),
-	.stop_data_load_flag	(stop_data_load_flag),
-	.coefficient_in_I	  	(coefficient_in_I),
-	.coefficient_in_Q	  	(coefficient_in_Q),
-	.data_in_I				(sr_data_in_I),
-	.data_in_Q				(sr_data_in_Q),
-	.data_out_I				(data_out_I),
-	.data_out_Q				(data_out_Q)
+	
+	.loadDataFlag			(loadDataFlag),
+	.stopDataLoadFlag		(stopDataLoadFlag),
+	.dataInRe				(srDataInRe),
+	.dataInIm				(srDataInIm),
+	
+	.dataOutRe				(dataOutRe),
+	.dataOutIm				(dataOutIm)
 );
 
 
@@ -59,65 +59,143 @@ initial begin
 
 	// Set the init values. Then send the coefficients and then the data in
 	// a serial manner, one clock cycle at a time.
-	coefficient_in_I = 0;
-	coefficient_in_Q = 0;
-	sr_data_in_I = 0;
-	sr_data_in_Q = 0;
+	srDataInRe = 0;
+	srDataInIm = 0;
 
-	load_coefficients_flag = 0;
-	load_data_flag = 0;
-	stop_data_load_flag = 0;
+	loadDataFlag = 0;
+	stopDataLoadFlag = 0;
 	repeat(RST_CYCLES) @ (posedge clock);
-
-	// Set the coiefficient values. Make sure the number of coefficients is
-	// equal to the set length of the n_tap_fir module. In this case there are
-	// 4 set values,
-	load_coefficients_flag = 1;
-	repeat(1) @ (posedge clock);
-	coefficient_in_I = 8'd3;
-	coefficient_in_Q = 8'd7;
-	repeat(1) @ (posedge clock);
-	coefficient_in_I = 8'd2;
-	coefficient_in_Q = 8'd0;
-	repeat(1) @ (posedge clock);
-	coefficient_in_I = 8'd17;
-	coefficient_in_Q = 8'd5;
-	repeat(1) @ (posedge clock);
-	coefficient_in_I = 8'd0;
-	coefficient_in_Q = -8'd3;
 	repeat(20) @ (posedge clock);
 
-	load_data_flag = 1;
+
+	loadDataFlag = 1;
 
 	// Set the input data values, in this case there are 4 values.
 	repeat(5) @ (posedge clock);
-	sr_data_in_I = 8'd2;
-	sr_data_in_Q = 8'd3;
+	srDataInRe = 8'd2;
+	srDataInIm = 8'd3;
 	repeat(1) @ (posedge clock);
-	sr_data_in_I = 8'd5;
-	sr_data_in_Q = 8'd10;
+	srDataInRe = 8'd5;
+	srDataInIm = 8'd10;
 	repeat(1) @ (posedge clock);
-	sr_data_in_I = -8'd2;
-	sr_data_in_Q = -8'd3;
+	srDataInRe = -8'd2;
+	srDataInIm = -8'd3;
 	repeat(1) @ (posedge clock);
-	sr_data_in_I = 8'd0;
-	sr_data_in_Q = -8'd6;
+	srDataInRe = 8'd0;
+	srDataInIm = -8'd6;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd56;
+	srDataInIm = -8'd39;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd55;
+	srDataInIm = 8'd110;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd21;
+	srDataInIm = -8'd93;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd46;
+	srDataInIm = -8'd54;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd76;
+	srDataInIm = 8'd8;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd5;
+	srDataInIm = 8'd10;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd15;
+	srDataInIm = -8'd96;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = -8'd75;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd12;
+	srDataInIm = 8'd98;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd78;
+	srDataInIm = 8'd100;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd20;
+	srDataInIm = -8'd30;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd99;
+	srDataInIm = -8'd69;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd21;
+	srDataInIm = 8'd32;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd53;
+	srDataInIm = 8'd107;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd28;
+	srDataInIm = -8'd33;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = -8'd66;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd26;
+	srDataInIm = 8'd38;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd57;
+	srDataInIm = 8'd107;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd25;
+	srDataInIm = -8'd35;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd80;
+	srDataInIm = -8'd96;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd42;
+	srDataInIm = 8'd3;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd1;
+	srDataInIm = 8'd1;
+	repeat(1) @ (posedge clock);
+	srDataInRe = -8'd32;
+	srDataInIm = -8'd23;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd90;
+	srDataInIm = -8'd96;
 	repeat(1) @ (posedge clock);
 
 	// Need to add [length number entered to the n_tap_complex_fir module - 1], in thus case
 	// that is 3 padded 0s. This is required by convolution opperation that the FIR
 	// filters achives.
-	sr_data_in_I = 8'd0;
-	sr_data_in_Q = 8'd0;
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
 	repeat(1) @ (posedge clock);
-	sr_data_in_I = 8'd0;
-	sr_data_in_Q = 8'd0;
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
 	repeat(1) @ (posedge clock);
-	sr_data_in_I = 8'd0;
-	sr_data_in_Q = 8'd0;
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
+	repeat(1) @ (posedge clock);
+	srDataInRe = 8'd0;
+	srDataInIm = 8'd0;
 	repeat(1) @ (posedge clock);
 
-	stop_data_load_flag =1 ;
+
+	//stopDataLoadFlag = 1;
 end
 
 initial begin
