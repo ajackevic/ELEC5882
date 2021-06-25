3dbfd0208423f780d339d5da8c098668f5afb5ac
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-25 10:05:15 +0100

    Removed the script to-do paragraph.

--- Verilog scripts/hilbert_transform/setup_HT_coeff.v
+++ Verilog scripts/hilbert_transform/setup_HT_coeff.v
@@ -87,11 +87,4 @@ always @(posedge clock) begin
 
 end
 
-
-/* This shoud be done in a normal way. As in have the setting of the
-   coefficients in this script. The loading of the matched filter coefficients
-	should be done on a MIF file. This script should follow very simillar logic
-	as the other setup_coeff scripts.
-*/
-
 endmodule
