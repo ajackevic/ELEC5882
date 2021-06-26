524d0af084869231290b0e4a01ecb32e7625820c
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-26 16:14:14 +0100

    Seems my FSM state values were all 0. Hence they were changed to 0,1,2,3.

--- Verilog scripts/hilbert_transform/hilbert_transform.v
+++ Verilog scripts/hilbert_transform/hilbert_transform.v
@@ -34,9 +34,9 @@ wire [(DATA_WIDTH * 2) - 1:0] FIRDataOut;
 // Create the FSM.
 reg [2:0] state;
 localparam IDLE = 3'd0;
-localparam LOAD_FIR_COEFF = 3'd0;
-localparam MAIN_OPP = 3'd0;
-localparam STOP = 3'd0;
+localparam LOAD_FIR_COEFF = 3'd1;
+localparam MAIN_OPP = 3'd2;
+localparam STOP = 3'd3;
 
 
 
