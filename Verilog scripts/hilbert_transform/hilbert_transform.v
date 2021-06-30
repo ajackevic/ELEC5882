99bc5a6009836778ea87c628114c2a69d255520a
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-06-30 15:16:50 +0100

    Removed the massive paragraph were I wrote what I should be doing in the module. (info used by the designer).

--- Verilog scripts/hilbert_transform/hilbert_transform.v
+++ Verilog scripts/hilbert_transform/hilbert_transform.v
@@ -195,18 +195,4 @@ always @ (posedge clock) begin
 	endcase
 end
 
-
-
-/* Need to find out if the HT is only needed for the input signal or if it
-   will also be needed for the impulse response. Though I think it doesn't
-	really change the opperation of this script. An FSM is needed, the 
-	coefficients need to be loaded (pased on parameters). Need to call n_tap_fir,
-	perform the conv opperation, delay the real signal by n, make sure the two signals 
-	leave at the same time. Have an output flag when all the the data is passed,
-	as the conv will added n extra length to the signal that went througih the n_tap_fir.
-	Andhence ignore the data that is outputed after the flag is raised. If this is only
-	for the input signal, then the flag will never need to be raised as the input data
-	will always be coming.
-*/
-
 endmodule
