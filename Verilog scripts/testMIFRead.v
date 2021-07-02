d6796796bdcd5acc339476a61d61a6ec116adc45
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-02 16:10:49 +0100

    Added the inputs, outputs, and parameters to the module.

--- Verilog scripts/testMIFRead.v
+++ Verilog scripts/testMIFRead.v
@@ -1,6 +1,12 @@
-module testMIFRead(
+module testMIFRead #(
+	parameter LENGTH = 20000,
+	parameter DATA_WIDTH = 13
 
-
+)(
+	input clock,
+	input enable,
+	
+	output reg signed [DATA_WIDTH - 1:0] outputValue
 );
 
 
