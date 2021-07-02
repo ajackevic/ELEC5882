957ed2061b28909b92a11d8bc0b1c8a6ce2632d4
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-02 14:01:41 +0100

    Added extra commenting to the script.

--- MATLAB Scripts/MFImpulseCoeffMIF.m
+++ MATLAB Scripts/MFImpulseCoeffMIF.m
@@ -22,11 +22,16 @@ chirpFreqEnd = 50000;
 chirpDuration = 0.1;
 samplingFreqs = 1e5;
 
-
 % Sampling frequency of 100KHz for 0.1sec duration.
 tChirp = 0:1/samplingFreqs:chirpDuration-1/samplingFreqs;
 
-
-% Creating a linear chirp waveform 
+% Creating a linear chirp waveform with the set parameters.
 chirpWave = chirp(tChirp,chirpFreqStart,chirpDuration,chirpFreqEnd);
+
+% Impulse response of the matched filter. This is equal to the complex
+% conjugate time reversal of the transmitted signal (chirpWave).
 h_t = flip(conj(hilbert(chirpWave)));
+
+
+%%
+%The following script section is for the creation of the MIf file.
