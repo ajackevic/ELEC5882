bc686db984a886e29694092ffa658bbe0d9ba5cf
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-02 11:02:32 +0100

    Changed the wires to be signed.

--- Verilog scripts/complex_n_tap_fir/n_tap_complex_fir_tb.v
+++ Verilog scripts/complex_n_tap_fir/n_tap_complex_fir_tb.v
@@ -38,10 +38,10 @@ reg loadCoeff;
 
 
 // Note the range of reg signed [7:0] is [-128 to 127].
-wire [(DATA_WIDTH * 2) - 1:0] dataOutRe;
-wire [(DATA_WIDTH * 2) - 1:0] dataOutIm;
-wire [DATA_WIDTH - 1:0] coefficientOutRe;
-wire [DATA_WIDTH - 1:0] coefficientOutIm;
+wire signed [(DATA_WIDTH * 2) - 1:0] dataOutRe;
+wire signed [(DATA_WIDTH * 2) - 1:0] dataOutIm;
+wire signed [DATA_WIDTH - 1:0] coefficientOutRe;
+wire signed [DATA_WIDTH - 1:0] coefficientOutIm;
 
 
 
