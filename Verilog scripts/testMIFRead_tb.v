cc12e73b1a80980199136a09fe28561e14b42865
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-02 16:25:35 +0100

    Created a clock signal of 50 MHz that sets enableModule on after RST_CYCLES (10) clock cycles. This script was copied from one of the test benches,

--- Verilog scripts/testMIFRead_tb.v
+++ Verilog scripts/testMIFRead_tb.v
@@ -1,6 +1,50 @@
 module testMIFRead_tb;
 
 
+// Parameters for creating the 50MHz clock signal
+localparam NUM_CYCLES = 500;
+localparam CLOCK_FREQ = 50000000;
+localparam RST_CYCLES = 10;
+
+
+
+// Local parameters for the dut module.
+reg clock;
+reg enableModule;
+
+
+
+
+initial begin
+	clock = 1'd0;
+	enableModule = 1'd0;
+	// Set enableModule to 1 after RST_CYCLES clock cycles.
+	repeat(RST_CYCLES) @ (posedge clock);
+	enableModule = 1'd1;
+end
+
+
+
+
+// Clock parameters.
+real HALF_CLOCK_PERIOD = (1000000000.0/$itor(CLOCK_FREQ))/2.0;
+integer half_cycles = 0;
+
+
+
+
+// Create the clock toggeling and stop it simulation when half_cycles == (2*NUM_CYCLES).
+always begin
+	#(HALF_CLOCK_PERIOD);
+	clock = ~clock;
+	half_cycles = half_cycles + 1;
+
+	if(half_cycles == (2*NUM_CYCLES)) begin
+		$stop;
+	end
+end
+
+
 
 
 endmodule
