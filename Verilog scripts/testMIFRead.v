980735370b6627c9a948021bdc2084b63320ece6
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-03 15:27:27 +0100

    Created a new reg called coeffBufferCounter. This counter is used to set the corresponding coeffBuffer value from the MIFBuffer. MIFBuffer values have now been passed on to real/imag coeffBuffers.

--- Verilog scripts/testMIFRead.v
+++ Verilog scripts/testMIFRead.v
@@ -16,17 +16,20 @@ reg signed [DATA_WIDTH-1:0] MIFBuffer [0:(LENGTH * 2) - 1];
 
 reg signed [DATA_WIDTH-1:0] realCoeffBuffer [0:LENGTH - 1];
 reg signed [DATA_WIDTH-1:0] imagCoeffBuffer [0:LENGTH - 1];
-
+// Width is log2(LENGTH) rounded up
+reg [13:0] coeffBufferCounter; 
 
 initial begin
 	integer k;
-
+	
 	$readmemb("MFImpulseCoeff.mif", MIFBuffer);
 	
-	
+	coeffBufferCounter = 14'd0;
 	for (k = 0; k <= (LENGTH * 2) - 1 ; k = k + 2) begin
-		realCoeffBuffer[k] = {(DATA_WIDTH){1'd0}};
-		imagCoeffBuffer[k] = {(DATA_WIDTH){1'd0}};
+		realCoeffBuffer[coeffBufferCounter] = MIFBuffer[k];
+		imagCoeffBuffer[coeffBufferCounter] = MIFBuffer[k+1];
+		
+		coeffBufferCounter = coeffBufferCounter + 14'd1;
 	end
 end
 
