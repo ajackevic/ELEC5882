da71440d9f34cb496afb152fd2eee683e87ce49d
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-03 21:19:38 +0100

    Created the loading of the input data to the MIF file. This is identical to h_t but for the  input signal.

--- MATLAB Scripts/MFImpulseCoeffMIF.m
+++ MATLAB Scripts/MFImpulseCoeffMIF.m
@@ -8,11 +8,13 @@
 % Short Description
 % -----------------
 % This script creates a MIF file containting the mathced filter impulse
-% response coefficients. 10,000 coefficients are generated and loaded to
-% the MIF file. The generating to the coefficients h_t is identical to the
-% way its created in the script pulseCompressionFilter.m. The 2's
-% compliment binary values that are loaded to the created file are of 16
-% bit width.
+% response coefficients and its input data. 10,000 coefficients are generated 
+% and loaded to the MIF file, whilst 66000 data input values are set through 
+% for the input data. The generating to the coefficients h_t and the input signal
+% receivedSignal is identical to the way its created in the script 
+% pulseCompressionFilter.m. The values are muiltiplied by 1000 and rounded to make 
+% them integers, thus suitable for FPGA. 2'scompliment binary values are set to the
+% MIF files and are of bit width 16.
 %
 %
 
@@ -27,6 +29,11 @@ chirpFreqEnd = 50000;
 chirpDuration = 0.1;
 samplingFreqs = 1e5;
 
+
+
+%%
+% Creating the matched filter impulse response signal, h_t.
+
 % Sampling frequency of 100KHz for 0.1sec duration.
 tChirp = 0:1/samplingFreqs:chirpDuration-1/samplingFreqs;
 
@@ -39,8 +46,41 @@ chirpWave = chirp(tChirp,chirpFreqStart,chirpDuration,chirpFreqEnd);
 h_t = flip(conj(hilbert(chirpWave))) * 1000;
 
 
+
+
 %%
-% The following section is for the creation of the MIF file.
+% Creating the input signal, x_t.
+
+% Creating padding of zeros before and after the chirp wave. These
+% paddings are of 0.1 sec duration and consists of amplitude reduction of 0.9,
+% 0.8, 0.7, and 0.6.
+paddedChirpWaveAmp1   = [zeros(1,samplingFreqs/2), chirpWave * 1.0, zeros(1,samplingFreqs/2)];
+paddedChirpWaveAmp0_9 = [zeros(1,samplingFreqs/2), chirpWave * 0.9, zeros(1,samplingFreqs/2)];
+paddedChirpWaveAmp0_8 = [zeros(1,samplingFreqs/2), chirpWave * 0.8, zeros(1,samplingFreqs/2)];
+paddedChirpWaveAmp0_7 = [zeros(1,samplingFreqs/2), chirpWave * 0.7, zeros(1,samplingFreqs/2)];
+paddedChirpWaveAmp0_6 = [zeros(1,samplingFreqs/2), chirpWave * 0.6, zeros(1,samplingFreqs/2)];
+
+
+% Creating chirp signals with different amount of noise.
+chirp1 = paddedChirpWaveAmp1;
+chirp2 = awgn(paddedChirpWaveAmp0_9,15);
+chirp3 = awgn(paddedChirpWaveAmp0_8,10);
+chirp4 = awgn(paddedChirpWaveAmp0_7,5);
+chirp5 = awgn(paddedChirpWaveAmp0_6,0.001);
+chirp6 = awgn(paddedChirpWaveAmp1,-5);
+
+
+% Summing up the different chirp waveforms into one continous long input signal.
+receivedSignal = [chirp1, chirp2, chirp3, chirp4, chirp5, chirp6];
+receivedSignal = receivedSignal * 1000;
+
+
+
+
+
+
+%%
+% The following section is for the creation of the h_t MIF file.
 
 % Name of the MIF file.
 MIFFile = 'MFImpulseCoeff.mif';
@@ -55,6 +95,32 @@ fileID = fopen(MIFFile,'w');
 for i = 1:1:length(h_t)
     fprintf(fileID,'%s\n', dec2bin(round(real(h_t(i))),16));
     fprintf(fileID,'%s\n', dec2bin(round(imag(h_t(i))),16));
+end
+
+
+% Close the opened MIF file.
+fclose(fileID);
+
+
+
+
+
+
+%%
+% The following section is for the creation of the x_t MIF file.
+
+% Name of the MIF file.
+MIFFile = 'MFInputData.mif';
+
+% Create or open (if file already exsists) the MIF file.
+fileID = fopen(MIFFile,'w');
+
+
+% Print the input data (x_t) to the MIF file. The values that are printed
+% are the rounded to int values. They are printed in 2's
+% compliment format. The length of each value is 16 bits.
+for i = 1:1:length(receivedSignal)
+    fprintf(fileID,'%s\n', dec2bin(round(receivedSignal(i)),16));
 end
 
 
