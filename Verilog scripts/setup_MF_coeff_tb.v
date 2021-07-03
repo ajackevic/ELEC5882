0638929a2333560a261083d9f872ddabc58ac229
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-03 16:59:01 +0100

    Added extra commenting to the script.

--- Verilog scripts/setup_MF_coeff_tb.v
+++ Verilog scripts/setup_MF_coeff_tb.v
@@ -1,29 +1,54 @@
+/*
+
+ setup_MF_coeff_tb.v
+ --------------
+ By: Augustas Jackevic
+ Date: June 2021
+
+ Module Description:
+ -------------------
+ This module is a test bench for the module setup_MF_coeff. It connects to the instantiated 
+ module. The test bench as of now does not do any self-testing, only observing the signals 
+ in ModelSim. It is vital that the MIF file is placed in <project directory>\simulation\modelsim, 
+ otherwise ModelSim will no read the MIF data.
+
+
+*/
+
+
+
 // Setting the time unit for this module.
 `timescale 1 ns/100 ps
-
 
 
 module setup_MF_coeff_tb;
 
 
-// Parameters for creating the 50MHz clock signal
+
+
+// Parameters for creating the 50MHz clock signal.
 localparam NUM_CYCLES = 50000;
 localparam CLOCK_FREQ = 50000000;
 localparam RST_CYCLES = 10;
 
+// Parameters for the dut module.
 localparam LENGTH = 10000;
 localparam DATA_WIDTH = 16;
+
+
 
 
 // Local parameters for the dut module.
 reg clock;
 reg enableModule;
-
 wire signed [DATA_WIDTH-1:0] outputValueRe;
 wire signed [DATA_WIDTH-1:0] outputValueIm;
 wire coeffSetFlag;
 
 
+
+
+// Setting the init values.
 initial begin
 	clock = 1'd0;
 	enableModule = 1'd0;
@@ -35,7 +60,7 @@ end
 
 
 
-
+// Connecting the instantiated dut module.
 setup_MF_coeff #(
 	.LENGTH 			(LENGTH),
 	.DATA_WIDTH 	(DATA_WIDTH)
@@ -58,7 +83,6 @@ integer half_cycles = 0;
 
 
 
-
 // Create the clock toggeling and stop it simulation when half_cycles == (2*NUM_CYCLES).
 always begin
 	#(HALF_CLOCK_PERIOD);
@@ -69,8 +93,5 @@ always begin
 		$stop;
 	end
 end
-
-
-
 
 endmodule
