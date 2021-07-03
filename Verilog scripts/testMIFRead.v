014978cdb570605674d4c33300a31390ca4ea428
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-03 15:48:23 +0100

    Created the reg state (also set its init value to 0) and the local parameters for the FSM.

--- Verilog scripts/testMIFRead.v
+++ Verilog scripts/testMIFRead.v
@@ -19,12 +19,24 @@ reg signed [DATA_WIDTH-1:0] imagCoeffBuffer [0:LENGTH - 1];
 // Width is log2(LENGTH) rounded up
 reg [13:0] coeffBufferCounter; 
 
+
+
+reg [1:0] state;
+localparam IDLE = 2'0;
+localparam MOVE_COEFF = 2'd1;
+localparam STOP = 2'd2;
+localparam EMPTY_STATE = 2'd3;
+
+
+
+
 initial begin: initValues
 	integer k;
 	
 	$readmemb("MFImpulseCoeff.mif", MIFBuffer);
 	
 	coeffBufferCounter = 14'd0;
+	state = IDLE;
 	for (k = 0; k <= (LENGTH * 2) - 1 ; k = k + 2) begin
 		realCoeffBuffer[coeffBufferCounter] = MIFBuffer[k];
 		imagCoeffBuffer[coeffBufferCounter] = MIFBuffer[k+1];
