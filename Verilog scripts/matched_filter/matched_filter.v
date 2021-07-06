9b9f1fdadb5d371c7a0c1b788534ffa9ae3e7fc3
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-06 10:28:58 +0100

    Removed the big paragraph comment used for outlaying what to do in the script.

--- Verilog scripts/matched_filter/matched_filter.v
+++ Verilog scripts/matched_filter/matched_filter.v
@@ -133,7 +133,6 @@ read_MIF_file #(
 
 
 
-
 // Instantiating the module n_tap_complex_fir. This is used for the main opperation of the matched filter
 // between the complex input data and the complex impulse reponse of the matched filter.
 n_tap_complex_fir #(
@@ -153,7 +152,6 @@ n_tap_complex_fir #(
 	.dataOutRe				(MFOutputRe),
 	.dataOutIm				(MFOutputIm)
 );
-
 
 
 
@@ -227,15 +225,5 @@ always @ (posedge clock) begin
 		
 	endcase
 end
-
-/*
- Right in this script I need to instantiate the n_tap_complex_fir module and setup_MF_coeff.
- I then need to pass on coefficient values from setup_MF_coeff to n_tap_complex_fir. Then I 
- need edit the script setup_MF_coeff so that its compatiable with two types of MIF file
- (have it based on pre set parameters). Hence that module can be used to read two MIF files,
- thus will need two instantiations. Once the coefficients are set (could potentially be done 
- in parallel), send through the x_t data to n_tap_complex_fir. The output should then go 
- through an ABS algorithm (obtains the absloute value of y_t).
-*/
 
 endmodule
