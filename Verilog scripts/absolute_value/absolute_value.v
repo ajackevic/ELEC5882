48ff37293e7ae9eb64a32b7aa66075c1720eb45b
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-07 10:40:46 +0100

    Added the the parameters and input/output ports to the module.

--- Verilog scripts/absolute_value/absolute_value.v
+++ Verilog scripts/absolute_value/absolute_value.v
@@ -1,7 +1,27 @@
-module  absolute_value(
-
-
+module  absolute_value #(
+	parameter DATA_WIDTH = 18
+) (
+	input clock,
+	input enable,
+	input signed [DATA_WIDTH - 1:0] dataIn,
+	
+	output reg signed [DATA_WIDTH - 1:0] dataOut
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 endmodule
