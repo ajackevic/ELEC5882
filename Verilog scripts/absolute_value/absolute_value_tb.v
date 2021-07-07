84cac0f8cc5c0066dc18ac5cfbcfa420b3fcdd13
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-07 11:03:48 +0100

    Created a 50MHz clock signal. This was copied from the script read_MIF_file_tb.v.

--- Verilog scripts/absolute_value/absolute_value_tb.v
+++ Verilog scripts/absolute_value/absolute_value_tb.v
@@ -1,4 +1,59 @@
+// Setting the time unit for this module.
+`timescale 1 ns/100 ps
+
+
 module absolute_value_tb;
+
+
+
+// Parameters for creating the 50MHz clock signal.
+localparam NUM_CYCLES = 1000000;
+localparam CLOCK_FREQ = 50000000;
+localparam RST_CYCLES = 10;
+
+
+
+
+// Local parameters for the dut module.
+reg clock;
+
+
+
+
+// Setting the init values.
+initial begin
+	clock = 1'd0;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Clock parameters.
+real HALF_CLOCK_PERIOD = (1000000000.0/$itor(CLOCK_FREQ))/2.0;
+integer half_cycles = 0;
+
+
+
+// Create the clock toggeling and stop it simulation when half_cycles == (2*NUM_CYCLES).
+always begin
+	#(HALF_CLOCK_PERIOD);
+	clock = ~clock;
+	half_cycles = half_cycles + 1;
+
+	if(half_cycles == (2*NUM_CYCLES)) begin
+		$stop;
+	end
+end
+
 
 
 
