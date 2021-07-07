237491c3521b7e6566c2e281c876fe9513ffb104
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-07 10:58:21 +0100

    Added commenting to the script.

--- Verilog scripts/absolute_value/absolute_value.v
+++ Verilog scripts/absolute_value/absolute_value.v
@@ -9,6 +9,8 @@ module  absolute_value #(
 );
 
 
+
+// Setting the output to an initial value of 0. 
 initial begin
 	dataOut <= {(DATA_WIDTH){1'd0}};
 end
@@ -16,7 +18,12 @@ end
 
 
 always @ (posedge clock) begin
+
+	// If enable is set, do the following. Else set the output to 0.
 	if(enable) begin
+	
+		// Check the MSB bit of dataIn. If 1, the value is negative, hence dataOut should be 
+		// set to -dataIn, else dataOut should be equal to dataIn
 		if(dataIn[DATA_WIDTH - 1] == 1'd1) begin
 			dataOut <= -dataInl;
 		end
