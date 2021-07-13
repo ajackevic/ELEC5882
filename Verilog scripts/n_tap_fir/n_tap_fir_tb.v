51c718762c6beb17989de7a7a0858ece907170f1
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-13 16:59:46 +0100

    Added an if/else statement in the else in FIR_MAIN. This checks if coeffSetFlag is set, if so, set enableFIRCoeff to 0 else set it to 1.

--- Verilog scripts/n_tap_fir/n_tap_fir_tb.v
+++ Verilog scripts/n_tap_fir/n_tap_fir_tb.v
@@ -239,7 +239,13 @@ always @(posedge clock) begin
 				state <= STOP;
 			end
 			else begin
-				enableFIRCoeff <= 1'd0;
+			
+				if(coeffSetFlag) begin
+					enableFIRCoeff <= 1'd0;
+				end
+				else begin
+					enableFIRCoeff <= 1'd1;
+				end
 				
 				dataIn <= dataInBuff[dataInCounter];
 				dataInCounter <= dataInCounter + 8'd1;
