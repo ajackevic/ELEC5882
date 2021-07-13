11c71a737864cf15a8d1637f99f720a2ff2a16f3
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-13 16:26:30 +0100

    Changed NUMB_DATAIN from 40 to 60 and set the 60 values of dataIn.

--- Verilog scripts/n_tap_fir/n_tap_fir_tb.v
+++ Verilog scripts/n_tap_fir/n_tap_fir_tb.v
@@ -27,10 +27,10 @@ localparam RST_CYCLES = 10;
 
 // Parameters for the dut module.
 localparam TAPS = 20;
-localparam DATA_WIDTH = 8;
+localparam DATA_WIDTH = 18;
 
 // Parameter for the number of data inputs.
-localparam NUMB_DATAIN = 40;
+localparam NUMB_DATAIN = 60;
 
 
 //
@@ -123,6 +123,70 @@ end
 // Set the initial value of the clock and dataInBuff and expectedDataOutBuff.
 initial begin
 	clock <= 0;
+	
+	dataInBuff[0]  <= 18'd131071;
+	dataInBuff[1]  <= 18'd131071;
+	dataInBuff[2]  <= 18'd131071;
+	dataInBuff[3]  <= 18'd131071;
+	dataInBuff[4]  <= 18'd131071;
+	dataInBuff[5]  <= 18'd131071;
+	dataInBuff[6]  <= 18'd131071;
+	dataInBuff[7]  <= 18'd131071;
+	dataInBuff[8]  <= 18'd131071;
+	dataInBuff[9]  <= 18'd131071;
+	dataInBuff[10] <= 18'd131071;
+	dataInBuff[11] <= 18'd131071;
+	dataInBuff[12] <= 18'd131071;
+	dataInBuff[13] <= 18'd131071;
+	dataInBuff[14] <= 18'd131071;
+	dataInBuff[15] <= 18'd131071;
+	dataInBuff[16] <= 18'd131071;
+	dataInBuff[17] <= 18'd131071;
+	dataInBuff[18] <= 18'd131071;
+	dataInBuff[19] <= 18'd131071;
+	
+	dataInBuff[20] <= -18'd131072;
+	dataInBuff[21] <= -18'd131072;
+	dataInBuff[22] <= -18'd131072;
+	dataInBuff[23] <= -18'd131072;
+	dataInBuff[24] <= -18'd131072;
+	dataInBuff[25] <= -18'd131072;
+	dataInBuff[26] <= -18'd131072;
+	dataInBuff[27] <= -18'd131072;
+	dataInBuff[28] <= -18'd131072;
+	dataInBuff[29] <= -18'd131072;
+	dataInBuff[30] <= -18'd131072;
+	dataInBuff[31] <= -18'd131072;
+	dataInBuff[32] <= -18'd131072;
+	dataInBuff[33] <= -18'd131072;
+	dataInBuff[34] <= -18'd131072;
+	dataInBuff[35] <= -18'd131072;
+	dataInBuff[36] <= -18'd131072;
+	dataInBuff[37] <= -18'd131072;
+	dataInBuff[38] <= -18'd131072;
+	dataInBuff[39] <= -18'd131072;
+	
+	dataInBuff[40] <= 18'd131071;
+	dataInBuff[41] <= 18'd0;
+	dataInBuff[42] <= -18'd17923;
+	dataInBuff[43] <= -18'd666;
+	dataInBuff[44] <= 18'd999
+	dataInBuff[45] <= -18'd12361;
+	dataInBuff[46] <= -18'd1;
+	dataInBuff[47] <= 18'd1251;
+	dataInBuff[48] <= 18'd48302;
+	dataInBuff[49] <= 18'd8592;
+	dataInBuff[50] <= 18'd22341;
+	dataInBuff[51] <= -18'd55555;
+	dataInBuff[52] <= -18'd32123;
+	dataInBuff[53] <= -18'd9898;
+	dataInBuff[54] <= 18'd23411;
+	dataInBuff[55] <= -18'd23211;
+	dataInBuff[56] <= 18'd992;
+	dataInBuff[57] <= 18'd73;
+	dataInBuff[58] <= -18'd124;
+	dataInBuff[59] <= -18'd1231;
+	
 end
 
 
