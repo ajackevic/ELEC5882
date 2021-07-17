46d8d18ee74800a2378f81d87995bb904766c982
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-17 10:38:22 +0100

    Added the bit shift (to the left) by 1 for absBetaValue in both if and else statements.

--- MATLAB Scripts/ABScomparison.m
+++ MATLAB Scripts/ABScomparison.m
@@ -23,9 +23,13 @@ for i = 1:1:dataLength
     if(abs(real(complexData(i))) >= abs(imag(complexData(i))))
        absAlphaValue = abs(real(complexData(i)));
        absBetaValue = abs(imag(complexData(i)));
+       
+       absBetaValue = bitshift(absBetaValue,-1);
     else
        absAlphaValue = abs(imag(complexData(i)));
        absBetaValue = abs(real(complexData(i)));
+       
+       absBetaValue = bitshift(absBetaValue,-1);
     end
 end
 
