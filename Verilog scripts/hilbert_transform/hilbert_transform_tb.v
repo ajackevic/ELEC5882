ea06c2f54b8343e3a55ec7b31f8489d51e3b2526
ajackevic <57046549+ajackevic@users.noreply.github.com>
2021-07-28 16:13:20 +0100

    Created the variable expectedOutBufIm and set its values in an init block.

--- Verilog scripts/hilbert_transform/hilbert_transform_tb.v
+++ Verilog scripts/hilbert_transform/hilbert_transform_tb.v
@@ -21,21 +21,23 @@ localparam NUM_CYCLES = 500;
 localparam CLOCK_FREQ = 50000000;
 localparam RST_CYCLES = 20;
 localparam LENGTH = 27;
-localparam DATA_WIDTH = 18;
+localparam DATA_WIDTH = 12;
 
 
 
 
-// Creating the lcoal parameters.
+// Creating the local parameters.
 reg clock;
 reg enable;
 reg stopDataInFlag;
 reg signed [DATA_WIDTH - 1:0] dataIn;
 wire signed [(DATA_WIDTH * 2) - 1:0] dataOutRe;
 wire signed [(DATA_WIDTH * 2) - 1:0] dataOutIm;
+
+
 reg signed [DATA_WIDTH - 1: 0] dataInBuf [0:29];
-
-
+reg signed [DATA_WIDTH - 1: 0] expectedOutBufRe [0:29];
+reg signed [DATA_WIDTH - 1: 0] expectedOutBufIm [0:29];
 
 
 // FSM states.
@@ -65,37 +67,102 @@ initial begin
 	enable = 1'd0;
 	stopDataInFlag = 1'd0;
 	
-	dataInBuff[0] = -18'd123;
-	dataInBuff[1] = 18'd12111;
-	dataInBuff[2] = 18'd891;
-	dataInBuff[3] = 18'd9;
-	dataInBuff[4] = 18'd0;
-	dataInBuff[5] = 18'd511;
-	dataInBuff[6] = 18'd1241;
-	dataInBuff[7] = -18'd7819;
-	dataInBuff[8] = -18'd76;
-	dataInBuff[9] = 18'd1111;
-	dataInBuff[10] = 18'd9861;
-	dataInBuff[11] = -18'd90;
-	dataInBuff[12] = -18'd8191;
-	dataInBuff[13] = -18'd88910;
-	dataInBuff[14] = 18'd888;
-	dataInBuff[15] = -18'd9901;
-	dataInBuff[16] = 18'd12;
-	dataInBuff[17] = 18'd11111;
-	dataInBuff[18] = -18'd1231;
-	dataInBuff[19] = -18'd131072;
-	dataInBuff[20] = -18'd131072;
-	dataInBuff[21] = -18'd131072;
-	dataInBuff[22] = 18'd89700;
-	dataInBuff[23] = -18'd12;
-	dataInBuff[24] = 18'd35111;
-	dataInBuff[25] = -18'd78819;
-	dataInBuff[26] = 18'd1;
-	dataInBuff[27] = 18'd99719;
-	dataInBuff[28] = 18'd999;
-	dataInBuff[29] = -18'd666;
-
+	dataInBuff[0] = -12'd123;
+	dataInBuff[1] = 12'd2000;
+	dataInBuff[2] = 12'd891;
+	dataInBuff[3] = 12'd9;
+	dataInBuff[4] = 12'd0;
+	dataInBuff[5] = 12'd511;
+	dataInBuff[6] = 12'd1241;
+	dataInBuff[7] = -12'd1567;
+	dataInBuff[8] = -12'd76;
+	dataInBuff[9] = 12'd1111;
+	dataInBuff[10] = 12'd154;
+	dataInBuff[11] = -12'd90;
+	dataInBuff[12] = -12'd1239;
+	dataInBuff[13] = -12'd1111;
+	dataInBuff[14] = 12'd888;
+	dataInBuff[15] = -12'd653;
+	dataInBuff[16] = 12'd12;
+	dataInBuff[17] = 12'd462;
+	dataInBuff[18] = -12'd1231;
+	dataInBuff[19] = -12'd1047;
+	dataInBuff[20] = -12'd2044;
+	dataInBuff[21] = -12'd2047;
+	dataInBuff[22] = 12'd897;
+	dataInBuff[23] = -12'd12;
+	dataInBuff[24] = 12'd111;
+	dataInBuff[25] = -12'd9;
+	dataInBuff[26] = 12'd1;
+	dataInBuff[27] = 12'd719;
+	dataInBuff[28] = 12'd999;
+	dataInBuff[29] = -12'd666;
+	
+	
+	
+	
+	expectedOutBufRe[0] = -36'd123;
+	expectedOutBufRe[1] = 36'd2000;
+	expectedOutBufRe[2] = 36'd891;
+	expectedOutBufRe[3] = 36'd9;
+	expectedOutBufRe[4] = 36'd0;
+	expectedOutBufRe[5] = 36'd511;
+	expectedOutBufRe[6] = 36'd1241;
+	expectedOutBufRe[7] = -36'd1567;
+	expectedOutBufRe[8] = -36'd76;
+	expectedOutBufRe[9] = 36'd1111;
+	expectedOutBufRe[10] = 36'd154;
+	expectedOutBufRe[11] = -36'd90;
+	expectedOutBufRe[12] = -36'd1239;
+	expectedOutBufRe[13] = -36'd1111;
+	expectedOutBufRe[14] = 36'd888;
+	expectedOutBufRe[15] = -36'd653;
+	expectedOutBufRe[16] = 36'd12;
+	expectedOutBufRe[17] = 36'd462;
+	expectedOutBufRe[18] = -36'd1231;
+	expectedOutBufRe[19] = -36'd1047;
+	expectedOutBufRe[20] = -36'd2044;
+	expectedOutBufRe[21] = -36'd2047;
+	expectedOutBufRe[22] = 36'd897;
+	expectedOutBufRe[23] = -36'd12;
+	expectedOutBufRe[24] = 36'd111;
+	expectedOutBufRe[25] = -36'd9;
+	expectedOutBufRe[26] = 36'd1;
+	expectedOutBufRe[27] = 36'd719;
+	expectedOutBufRe[28] = 36'd999;
+	expectedOutBufRe[29] = -36'd666;
+	
+	
+	expectedOutBufIm[0] = -36'3075;
+	expectedOutBufIm[1] = -36'50000;
+	expectedOutBufIm[2] = -36'16002;
+	expectedOutBufIm[3] = -36'102225;
+	expectedOutBufIm[4] = -36'33141;
+	expectedOutBufIm[5] = -36'213234;
+	expectedOutBufIm[6] = -36'97862;
+	expectedOutBufIm[7] = -36'349786;
+	expectedOutBufIm[8] = -36'183179;
+	expectedOutBufIm[9] = -36'642587;
+	expectedOutBufIm[10] = -36'333333;
+	expectedOutBufIm[11] = -36'1241091;
+	expectedOutBufIm[12] = -36'501670;
+	expectedOutBufIm[13] = -36'4000755;
+	expectedOutBufIm[14] = -36'2405268;
+	expectedOutBufIm[15] = 36'4082876;
+	expectedOutBufIm[16] = 36'1021736;
+	expectedOutBufIm[17] = 36'1005284;
+	expectedOutBufIm[18] = -36'1824225;
+	expectedOutBufIm[19] = 36'4445650;
+	expectedOutBufIm[20] = 36'3175030;
+	expectedOutBufIm[21] = -36'4131095;
+	expectedOutBufIm[22] = 36'1150050;
+	expectedOutBufIm[23] = 36'2840122;
+	expectedOutBufIm[24] = 36'3065563;
+	expectedOutBufIm[25] = 36'3100240;
+	expectedOutBufIm[26] = -36'3291358;
+	expectedOutBufIm[27] = -36'403851;
+	expectedOutBufIm[28] = 36'2402470;
+	expectedOutBufIm[29] = -36'1608079;
 	
 	
 	repeat(RST_CYCLES) @ (posedge clock);
